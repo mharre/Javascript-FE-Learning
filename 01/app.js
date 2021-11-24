e69ed2ea99f56fdff0eb99291526240aa9d3b770
unknown <matthewharre@gmail.com>
2021-11-24 03:29:07 -0800

    added more basic js examples

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,52 @@
-alert('Hello');
+// Log to Console, any datatypes can be logged
+//  can also log objects, arrays etc
+
+console.log('Hello World'); 
+console.log([1,2,3]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+
+console.time('Identifier')
+    console.log('Hello World'); 
+    console.log('Hello World'); 
+    console.log('Hello World'); 
+    console.log('Hello World'); 
+    console.log('Hello World'); 
+    console.log('Hello World'); 
+console.timeEnd('Identifier');
+
+console.clear();
+
+// var, let, const for variables
+
+var name = 'Test'
+console.log(name);
+name = 'testttt'
+console.log(name);
+
+// init var, undefined and can use later
+
+var greeting;
+console.log(greeting);
+greeting = 'hello'
+console.log(greeting);
+
+// letters, numbers, _, $ can only be used in var names
+// let works similar to var, just different block level scope
+// const works differently, it cant change or be reassigned
+// MUST also assign a value
+
+const person = {
+    name: 'John',
+    age: 3
+}
+person.name = 'Sara'
+// console.log(person);
+
+// can change the object, but can't reassign const
+const numbers = [1,2,3,4]
+numbers.push(6);
+console.log(numbers);
