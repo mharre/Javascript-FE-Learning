61e68a7e3bd35b210d989e159eb13df357892ae7
unknown <matthewharre@gmail.com>
2021-11-29 09:16:12 -0800

    added more basic js examples

--- 01/app.js
+++ 01/app.js
@@ -785,4 +785,78 @@
 // }
 
 //EVENT BUBBLING
+// document.querySelector('.card-title').addEventListener('click',
+// function(){
+//     console.log('card title');
+// });
 
+// document.querySelector('.card-content').addEventListener('click',
+// function(){
+//     console.log('card content');
+// });
+
+// document.querySelector('.card').addEventListener('click',
+// function(){
+//     console.log('card');
+// });
+
+// document.querySelector('.col').addEventListener('click',
+// function(){
+//     console.log('col');
+// });
+
+// EVENT DELEGATION - putting listener on parent and going down
+// delItem only worked when clicking first x, doesn't work for others
+// must add delegation when adding something in dyamically too
+
+// const delItem = document.querySelector('.delete-item');
+// delItem.addEventListener('click', deleteItem);
+
+// document.body.addEventListener('click', deleteItem);
+
+// function deleteItem(e){
+    // if(e.target.parentElement.className === 'delete-item secondary-content'){
+    //     console.log('delete-item');
+    // }
+    // if(e.target.parentElement.classList.contains('delete-item')){
+        // console.log('delete-item');
+        // e.target.parentElement.parentElement.remove();
+    // }
+// }
+
+// set local storage item, local stays until manually clear
+// session leaves when browser is closed
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', 30);
+// sessionStorage.setItem('name', 'Bob');
+
+// localStorage.removeItem('name');
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
+
+// localStorage.clear();
+// console.log(name, age);
+document.querySelector('form').addEventListener('submit',
+function (e){
+    const task = document.getElementById('task').value;
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    alert('task saved!');
+
+    e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+tasks.forEach(function(task){
+    console.log(task);
+});
