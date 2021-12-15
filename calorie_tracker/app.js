4491c1cf245c6ebcf5b28e1199975ee25ed0673e
unknown <matthewharre@gmail.com>
2021-12-15 01:22:17 -0800

    added ability to add items to the ui

--- calorie_tracker/app.js
+++ calorie_tracker/app.js
@@ -22,6 +22,22 @@ const ItemCtrl = (function(){
         getItems: function(){
             return data.items;
         },
+        addItem: function(name, calories){
+            let ID;
+            // create it
+            if(data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1
+            } else {
+                ID = 0;                
+            }
+            // calories to number
+            calories = parseInt(calories); 
+            // create a new item
+            newItem = new Item(ID, name, calories);
+            data.items.push(newItem);
+
+            return newItem;
+        },  
         logData: function(){
             return data;
         }
@@ -31,7 +47,10 @@ const ItemCtrl = (function(){
 // ui controller
 const UICtrl = (function(){
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCalInput: '#item-calories'
     }
     // public methods
     return {
@@ -45,12 +64,38 @@ const UICtrl = (function(){
             });
             // inserting list items
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCalInput).value 
+            }
+        },
+        getSelectors: function(){
+            return UISelectors;
         }
     }
 })();
 
 // app controller
 const App = (function(ItemCtrl, UICtrl){
+    const loadEventListeners = function(){
+        const UISelectors = UICtrl.getSelectors();
+
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+    // add item submit
+    const itemAddSubmit = function(e){
+        // get form input from UI controller
+        const input = UICtrl.getItemInput();
+        // check for name / cal input
+        if(input.name !== '' && input.calories !== ''){
+            // add item
+            const newItem = ItemCtrl.addItem(input.name,input.calories);
+        }
+
+        e.preventDefault();
+    }
 
     // public methods
     return {
@@ -59,6 +104,8 @@ const App = (function(ItemCtrl, UICtrl){
             const items = ItemCtrl.getItems();
             // populate list with items
             UICtrl.populateItemList(items); 
+            // load even listners
+            loadEventListeners();
         }
     }
 })(ItemCtrl,UICtrl);
